5b6ce91278f05c2824bd26f80ea34f821b104115
VictorTehYJ <victortehyj@gmail.com>
2020-05-06 03:27:56 +0800

    rating and write a review function done.

--- application.py
+++ application.py
@@ -26,12 +26,12 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == 'GET':
-        if session.get("user_name") is None:
+        if session.get("user_id") is None:
             session["login"] = False
             return render_template("index.html", login=session["login"])
         else:
-            user_name = session["user_name"]
-            return render_template("search.html", user_name=user_name, login=session["login"])
+            user_id = session["user_id"]
+            return render_template("search.html", user_id=user_id, login=session["login"])
 
     else:
         # clear session if they submit a post request directly, be sure to log them out first
@@ -53,8 +53,8 @@ def index():
             db_password = user["password"]
             if bcrypt.check_password_hash(db_password, password):
                 session["login"] = True
-                session["user_name"] = user["name"]
-                return render_template("search.html", user_name=session["user_name"], login=session["login"])
+                session["user_id"] = user["id"]
+                return render_template("search.html", user_id=session["user_id"], login=session["login"])
             else:
                 message = "Invalid username or password"
                 return render_template("index.html", dander_message=message, login=session["login"])
@@ -88,8 +88,8 @@ def logout():
             db_password = user["password"]
             if bcrypt.check_password_hash(db_password, password):
                 session["login"] = True
-                session["user_name"] = user["name"]
-                return render_template("search.html", user_name=session["user_name"], login=session["login"])
+                session["user_id"] = user["name"]
+                return render_template("search.html", user_id=session["user_id"], login=session["login"])
             else:
                 message = "Invalid username or password"
                 return render_template("index.html", dander_message=message, login=session["login"])
@@ -108,10 +108,12 @@ def register():
         password = request.form.get("password").strip()
         name = request.form.get("name").strip()
 
-        if len(username) < 0 and len(password) < 0 and len(name) < 0:
-            return render_template("register.html", warning_message="Enter all required fields")
+        if len(username) <= 0 and len(password) <= 0 and len(name) <= 0:
+            message="Enter all required fields"
+            return render_template("register.html", warning_message=message)
         elif db.execute("SELECT username from users WHERE username=:username", {"username": username}).rowcount != 0:
-            return render_template("register.html", warning_message="Username taken")
+            message="Username taken"
+            return render_template("register.html", warning_message=message)
         else:
             # hashing password with bcrypt hashing
             pw_hash = bcrypt.generate_password_hash(password).decode('utf-8')
@@ -131,16 +133,36 @@ def search():
         search = "%" + request.form.get("search").lower() + "%"
         results = db.execute(
             "SELECT * FROM books WHERE lower(title) LIKE :search OR lower(author) LIKE :search OR lower(isbn) LIKE :search", {"search": search}).fetchall()
-        if results is None:
-            return render_template("search.html", danger_message="No such book.")
-        else:
-            return render_template("search.html", results=results, danger_message="No such book.")
 
-@app.route("/books/<int:book_id>")
+        if db.execute(
+            "SELECT * FROM books WHERE lower(title) LIKE :search OR lower(author) LIKE :search OR lower(isbn) LIKE :search", {"search": search}).rowcount==0:
+            message="No such book."
+            return render_template("search.html", danger_message=message)
+        else:
+            return render_template("search.html", results=results)
+
+
+@app.route("/books/<int:book_id>", methods=["GET", "POST"])
 def book(book_id):
     # Lists details about a single book.
-
     # Make sure book exists.
-    book = db.execute("SELECT * FROM books WHERE id = :id", {"id": book_id}).fetchone()
+    book = db.execute("SELECT * FROM books WHERE id = :id",
+                        {"id": book_id}).fetchone()
+    if request.method == 'GET':
+        return render_template("book.html", book=book)
+    else:
+        rating=-1
+        # get rating and review from form
+        rating = int(request.form.get("rating"))
+        review = request.form.get("review").strip()
 
-    return render_template("book.html", book=book)
+        if rating < 0 or len(review) <= 0:
+            message="Invalid Review. Please rate and write a review before submit."
+            return render_template("book.html", book=book, danger_message=message)
+        else:
+            db.execute("INSERT INTO reviews (user_id, book_id, review, rating) VALUES (:user_id, :book_id, :review, :rating)", {
+                       "user_id": session["user_id"], "book_id": book_id, "review": review, "rating": rating})
+            db.commit()
+            message="Review submitted successfully!"
+            return render_template("book.html", book=book, success_message=message)
+        
